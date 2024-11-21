63ab65d0a98b8c847bdb4813e4a1a8f7fe822733
Emander Dangla <emanderdangla@Emanders-Laptop.local>
2024-11-20 20:47:17 -0500

    update workspace

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,12 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
   backend "remote" {
-    hostname     = "app.terraform.io"
     organization = "emander-org"
     workspaces {
       name = "emander-vpn"

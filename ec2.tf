450837faf467271dcb5dcdeaff34e1b9f2882804
Emander Dangla <emanderdangla@Emanders-Laptop.local>
2024-11-20 21:41:06 -0500

    update workspace

--- ec2.tf
+++ ec2.tf
@@ -1,17 +1,16 @@
-resource "aws_key_pair" "emander-key" {
-  key_name   = "emander-key"
-  public_key = var.public_key
-}
-
-
 resource "aws_instance" "emander-instance" {
   ami           = "ami-0a14a0a5716389b2d"
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.deployer.key_name
+  key_name      = aws_key_pair.emander-key.key_name
 
   vpc_security_group_ids = [aws_security_group.emander_sg.id]
 
   tags = {
     Name = var.instance_name
   }
+}
+
+resource "aws_key_pair" "emander-key" {
+  key_name   = "emander-key"
+  public_key = var.public_key
 }
